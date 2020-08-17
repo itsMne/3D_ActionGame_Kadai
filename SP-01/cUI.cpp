953bc5174a7a6cb2796affb546d9d73f8c94c3a6
Mane <39531147+itsMne@users.noreply.github.com>
2020-08-18 03:34:48 +0900

    lock on effect

--- SP-01/cUI.cpp
+++ SP-01/cUI.cpp
@@ -8,6 +8,7 @@ enum UI_TEXTURES
 {
 	UI_PAUSE_TEX,
 	UI_FLOWER_TEX,
+	UI_LOCKON_TEX,
 	UI_TEX_MAX
 };
 ID3D11ShaderResourceView * pTextures[UI_TEX_MAX] = { nullptr };
@@ -65,6 +66,13 @@ void cUI::Init()
 		nAnimeFrameChange = 10;
 		x2UVFrame.y = nType;
 		break;
+	case UI_LOCKON:
+		if (!pTextures[UI_LOCKON_TEX])
+			CreateTextureFromFile(GetDevice(), "data/texture/Zoom.tga", &pTextures[UI_LOCKON_TEX]);
+		SetTexture(pTextures[UI_LOCKON_TEX]);
+		SetSize(1280, 50);
+		SetAlpha(0.5f);
+		break;
 	case UI_GAME_MANAGER:
 		for (int i = UI_HEALTH_FLOWER; i < UI_GAME_MANAGER; pUIs[i] = new cUI(i), i++);
 		break;
@@ -104,6 +112,12 @@ void cUI::Update()
 	case UI_HEALTH_FLOWER:
 		FlowerHealthUIControl();
 		break;
+	case UI_LOCKON:
+		if (GetPlayer() && GetPlayer()->GetLockedEnemy()) {
+			x2UVFrame.x += 0.005f;
+			SetPolygonUV(x2UVFrame.x / 1.0f, x2UVFrame.y / 1.0f);
+		}
+		break;
 	case UI_GAME_MANAGER:
 		for (int i = UI_HEALTH_FLOWER; i < UI_GAME_MANAGER; pUIs[i]->Update(), i++);
 		break;
@@ -141,12 +155,30 @@ void cUI::Draw()
 	switch (nType)
 	{
 	case UI_HEALTH_FLOWER:
-		for (int i = UI_HEALTH_FLOWER-1; i > 1; pHealthFlower[i]->Draw(), i--);
+		for (int i = UI_HEALTH_FLOWER - 1; i > 1; pHealthFlower[i]->Draw(), i--);
 		pHealthFlower[0]->Draw();
 		pHealthFlower[1]->Draw();
 		break;
 	case UI_GAME_MANAGER:
-		for (int i = UI_HEALTH_FLOWER; i < UI_GAME_MANAGER; pUIs[i]->Draw(), i++);
+		if (GetPlayer() && GetPlayer()->GetLockedEnemy()) {
+			pUIs[UI_LOCKON]->SetTexture(nullptr);
+			pUIs[UI_LOCKON]->SetAlpha(0.5f);
+			pUIs[UI_LOCKON]->SetPosition(0, SCREEN_HEIGHT / 2 - pUIs[UI_LOCKON]->GetSize().y / 2);
+			pUIs[UI_LOCKON]->Draw();
+			pUIs[UI_LOCKON]->SetPosition(0, -(SCREEN_HEIGHT / 2 - pUIs[UI_LOCKON]->GetSize().y / 2));
+			pUIs[UI_LOCKON]->Draw();
+			pUIs[UI_LOCKON]->SetTexture(pTextures[UI_LOCKON_TEX]);
+			pUIs[UI_LOCKON]->SetAlpha(1.0f);
+			pUIs[UI_LOCKON]->SetPosition(0, SCREEN_HEIGHT / 2 - pUIs[UI_LOCKON]->GetSize().y / 2);
+			pUIs[UI_LOCKON]->Draw();
+			pUIs[UI_LOCKON]->SetPosition(0, -(SCREEN_HEIGHT / 2 - pUIs[UI_LOCKON]->GetSize().y / 2));
+			pUIs[UI_LOCKON]->Draw();
+		}
+		for (int i = UI_HEALTH_FLOWER; i < UI_GAME_MANAGER; i++){
+			if (i == UI_LOCKON)
+				continue;
+			pUIs[i]->Draw();
+	}
 		break;
 	default:
 		Polygon2D::DrawPolygon(GetDeviceContext());
